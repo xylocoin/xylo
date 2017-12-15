c886864e57d8b3ae72f29a8110c6c276649cc3ee
Rob Kaandorp <Rob Kaandorp>
2017-12-15 15:51:35 +0100

    Temporarely set the for block height to 230000 for testing.

--- src/main.cpp
+++ src/main.cpp
@@ -1425,7 +1425,7 @@ const CBlockIndex* GetLastBlockIndex(const CBlockIndex* pindex, bool fProofOfSta
     return pindex;
 }
 
-const int targetReadjustment_forkBlockHeight = 300000;
+const int targetReadjustment_forkBlockHeight = 230000;
 
 unsigned int GetNextTargetRequired(const CBlockIndex* pindexLast, bool fProofOfStake)
 {
